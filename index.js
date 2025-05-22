8b14f98b619ca988dc416bc3a35f1e05346140da
Ariza-salaman <ydw25423@live.com>
2025-05-22 09:39:13 +0800

    2025052201

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const message = 'hello world'
 
-const btn =  document.querySelector('buton')
+const btn =  document.querySelector('button')
 
 btn.addEventListener('click',()=>{
     alert(message)
